4b2d4fd8365d0d6f18b399527667a328a20ddcc8
JoseLuzardo <josaluza@espol.edu.ec>
2021-08-15 11:48:12 -0500

    Creado el controlador del FXML vistaAdmin

---
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/VistaAdminController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package espol.proyectopoo_g8_p2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.layout.Pane;
+/**
+ * FXML Controller class
+ *
+ * @author andre
+ */
+public class VistaAdminController implements Initializable {
+
+
+    @FXML
+    private Pane panelMapa;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
