0fbfa12a1826a31607e430ec6df944f4102e2fc9
RicardoMolinaCoronel <84423995+RicardoMolinaCoronel@users.noreply.github.com>
2021-08-15 13:01:40 -0500

    Create Administrador.java

---
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Administrador.java
@@ -0,0 +1,9 @@
+package espol.proyectopoo_g8_p2;
+public class Administrador extends Usuario{
+    public void registroResidente(){
+        
+    }
+    public void generarReporte(){
+        
+    }
+}
