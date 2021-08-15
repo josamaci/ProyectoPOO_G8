78404b0afcbcae02dfdaa449e8c7a28db364cfec
josamaci <84406998+josamaci@users.noreply.github.com>
2021-08-15 11:38:41 -0500

    Update Principal.fxml

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/PrimaryController.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/PrimaryController.java
@@ -1,10 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package espol.proyectopoo_g8_p2;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class PrimaryController {
+/**
+ * FXML Controller class
+ *
+ * @author JMaci
+ */
+public class PrimaryController implements Initializable {
 
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
