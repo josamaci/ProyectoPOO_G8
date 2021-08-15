bcba8b2b8d3d96836c34300a89413dceeacff60f
RicardoMolinaCoronel <84423995+RicardoMolinaCoronel@users.noreply.github.com>
2021-08-15 12:48:45 -0500

    Update Usuario.java

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Usuario.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Usuario.java
@@ -1,1 +1,16 @@
+package espol.proyectopoo_g8_p2;
+public abstract class Usuario {
+    private String nombreUsuario;
+    private String contrasenia;
+    public Usuario(String nombreUsuario,String contrasenia){
+        this.nombreUsuario=nombreUsuario;
+        this.contrasenia=contrasenia;
+    }
+    public String getNombreUsuario(){
+        return nombreUsuario;
+    }
+    public String getContrasenia(){
+        return contrasenia;
+    }
+}
 
