f460a290786285fd46f6040b8c2ef20ce3166d03
josamaci <84406998+josamaci@users.noreply.github.com>
2021-08-15 11:31:31 -0500

    Update primary.fxml

--- ProyectoPOO_G8_P2/src/main/java/module-info.java
+++ ProyectoPOO_G8_P2/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module espol.proyectopoo_g8_p2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens espol.proyectopoo_g8_p2 to javafx.fxml;
     exports espol.proyectopoo_g8_p2;
