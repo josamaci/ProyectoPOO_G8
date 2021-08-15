9fbf77a237d08804b3299f99c10c1ec8281e2b6a
RicardoMolinaCoronel <84423995+RicardoMolinaCoronel@users.noreply.github.com>
2021-08-15 13:00:29 -0500

    Update Residente.java

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Residente.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Residente.java
@@ -1,1 +1,54 @@
+package espol.proyectopoo_g8_p2;
+public class Residente extends Usuario{
+    private String correo;
+    private String casa;
+    private Genero genero;
+    private String nombre;
+    private ArrayList<Vehiculo> vehiculos= new ArrayList();
+    private String cedula;
+    private String pinAcceso;
+            
+    public void mostrarInformacion(){
+        
+    }
+    public String cambiarPin(pinAcceso){
+        return pinAcceso;
+    }
+    public String cambiarPin(pinAcceso){
+        return pinAcceso;
+    }
+    public void registrarVehiculo(){
+        
+    }
+    public String registrarVisitante(){
+        return "";
+    }
+    public void listaVisitantes(){
+        
+    }
+    public void eliminarVisitante(){
+        
+    }
+    public String getCorreo(){
+        return correo;
+    }
+    public Genero getGenero(){
+        return genero;
+    }
+    public String getCasa(){
+        return casa;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public ArrayList<Vehiculo> getVehiculos(){
+        return vehiculos;
+    }
+    public String getCedula(){
+        return cedula;
+    }
+     public String getPinAcceso(){
+        return pinAcceso;
+    }
+    }
 
