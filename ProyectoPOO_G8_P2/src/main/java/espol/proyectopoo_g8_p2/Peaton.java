69c074ec5d7158133fee404665b551d91d1e98a5
josamaci <84406998+josamaci@users.noreply.github.com>
2021-08-16 18:49:13 -0500

    Update Peaton.java

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Peaton.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Peaton.java
@@ -1,9 +1,13 @@
-private String numcedula;
-private String pinAcceso;
+package espol.proyectopoo_g8_p2;
 
-public String getNumCedula(){
-return numcedula;
-}
-public String getPinAcceso(){
-return pinAcceso;
+public class Peaton{
+    private String numcedula;
+    private String pinAcceso;
+
+    public String getNumCedula(){
+    return numcedula;
+    }
+    public String getPinAcceso(){
+    return pinAcceso;
+    }
 }
