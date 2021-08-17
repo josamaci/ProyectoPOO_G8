ad2564cb061a0e9ef2ec65701837ba78a1355d1b
JoseLuzardo <josaluza@espol.edu.ec>
2021-08-16 20:15:33 -0500

    Update Administrador.java

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Administrador.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Administrador.java
@@ -1,4 +1,12 @@
 package espol.proyectopoo_g8_p2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Administrador extends Usuario{
     
     public Administrador(String nombreUsuario, String contrasenia){
@@ -9,5 +17,30 @@ public class Administrador extends Usuario{
     }
     public void generarReporte(){
         
+    }
+    
+    public static List<Administrador> cargarAdmin(){
+        
+            String ruta = "admin.txt";
+            List<Administrador> administradores = new ArrayList<>();
+            
+            try(InputStream input = App.class.getResource(ruta).openStream();
+                BufferedReader bf = new BufferedReader(
+                                    new InputStreamReader(input,"UTF-8"))){
+                
+                String linea;
+                
+                while((linea=bf.readLine())!=null){
+                    System.out.println(linea);
+                    String[] p = linea.split(",");
+                    
+                    Administrador administrador = new Administrador(p[0],p[1]);
+                    administradores.add(administrador);
+                }
+                
+            } catch (IOException ex){
+                System.out.println("ERROR: No se pudo cargar la información de los residentes");
+            }
+        return administradores;
     }
 }
