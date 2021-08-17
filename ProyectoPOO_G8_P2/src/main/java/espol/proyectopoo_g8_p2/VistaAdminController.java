aa63760e67a0bd59b153fa0ec3e8072792e715ec
JoseLuzardo <josaluza@espol.edu.ec>
2021-08-16 20:37:50 -0500

    Update VistaAdminController.java

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/VistaAdminController.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/VistaAdminController.java
@@ -5,12 +5,25 @@
  */
 package espol.proyectopoo_g8_p2;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 /**
  * FXML Controller class
  *
@@ -27,6 +40,18 @@ public class VistaAdminController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        List<Casa> casas = Casa.cargarCasa();
+        for(Casa c: casas){
+            Rectangle r = new Rectangle(50,50, Color.RED);
+            StackPane st = new StackPane();
+            st.getChildren().addAll(r);
+            
+            panelMapa.getChildren().addAll(st);
+            
+            st.setLayoutX(c.getCoordenadas().getX());
+            st.setLayoutY(c.getCoordenadas().getY());
+                   
+        }
     }    
     
 }
