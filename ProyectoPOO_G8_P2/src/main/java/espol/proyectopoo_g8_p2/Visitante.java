da1aa22e1f15f1a2fd3bfbcddcbdaba4fdb052e2
RicardoMolinaCoronel <84423995+RicardoMolinaCoronel@users.noreply.github.com>
2021-08-16 19:11:33 -0500

    Update Visitante.java

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Visitante.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Visitante.java
@@ -1,1 +1,14 @@
+package espol.proyectopoo_g8_p2;
+import java.time.LocalDateTime;
+private String codigoAcceso;
+private String nombreVisitante;
+private String numCedula;
+private int mzResidente;
+private int villaResidente;
+private LocalDateTime fechaIngreso;
+
+public void registrarVisita(String ca,String nv,int nc,int mz,int villa,LocalDateTime f){
+
+}
+
 
