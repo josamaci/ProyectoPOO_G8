795f91ad2d6e15e198df8fa2256d810730c938bd
josamaci <84406998+josamaci@users.noreply.github.com>
2021-08-21 12:13:24 -0500

    Update

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/VistaResidenteController.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/VistaResidenteController.java
@@ -12,7 +12,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 /**
  * FXML Controller class
  *
@@ -23,6 +29,58 @@ public class VistaResidenteController implements Initializable {
 
     @FXML
     private Button botonCerrar;
+    @FXML
+    private HBox vbnombre;
+    @FXML
+    private Label lblNombre;
+    @FXML
+    private HBox vbCorreo;
+    @FXML
+    private Label lblCorreo;
+    @FXML
+    private HBox vbVilla;
+    @FXML
+    private Label lblVilla;
+    @FXML
+    private HBox vbManzana;
+    @FXML
+    private Label lblManzana;
+    @FXML
+    private HBox vbCedula;
+    @FXML
+    private Label lblCedula;
+    @FXML
+    private HBox vbPin;
+    @FXML
+    private Label lblPin;
+    @FXML
+    private TextField txtPin;
+    @FXML
+    private Button botonPin;
+    @FXML
+    private GridPane gridVehiculo;
+    @FXML
+    private TextField txtVehiculo;
+    @FXML
+    private Button botonVehiculo;
+    @FXML
+    private TextField txtNombreVisitante;
+    @FXML
+    private TextField txtCedulaVisitante;
+    @FXML
+    private TextField txtCorreoVisitante;
+    @FXML
+    private TextField txtAnioVisita;
+    @FXML
+    private ComboBox<String> ComboMesVisita;
+    @FXML
+    private TextField txtDiaVisita;
+    @FXML
+    private ComboBox<Integer> ComboHoraVisita;
+    @FXML
+    private ComboBox<Integer> ComboMinutoVisita;
+    @FXML
+    private TableView<Visitante> tableVisitante;
     /**
      * Initializes the controller class.
      */
