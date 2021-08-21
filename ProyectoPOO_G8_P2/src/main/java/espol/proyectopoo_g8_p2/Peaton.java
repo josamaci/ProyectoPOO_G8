17a884dcaeffee04666077a75c249fdba9ad234d
RicardoMolinaCoronel <84423995+RicardoMolinaCoronel@users.noreply.github.com>
2021-08-21 16:44:36 -0500

    Update Peaton.java

--- ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Peaton.java
+++ ProyectoPOO_G8_P2/src/main/java/espol/proyectopoo_g8_p2/Peaton.java
@@ -3,11 +3,21 @@ package espol.proyectopoo_g8_p2;
 public class Peaton{
     private String numcedula;
     private String pinAcceso;
+    public Peaton(String numCedula, String pinAcceso){
+        this.numCedula=numCedula;
+        this.pinAcceso=pinAcceso;
+    }
 
     public String getNumCedula(){
     return numcedula;
     }
     public String getPinAcceso(){
     return pinAcceso;
+    }
+    public void setNumCedula(){
+    this.numCedula=numcedula;
+    }
+    public void setPinAcceso(){
+    this.pinAcceso=pinAcceso;
     }
 }
