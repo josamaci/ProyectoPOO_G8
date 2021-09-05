1f83c548e200f4d39dd14cfd228ba73af4a6cfe1
josamaci <84406998+josamaci@users.noreply.github.com>
2021-09-04 19:40:31 -0500

    Update

--- ProyectoPOO_G8_P2/src/main/java/module-info.java
+++ ProyectoPOO_G8_P2/src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module espol.proyectopoo_g8_p2 {
+module espol.proyectopoo_g8_p2{
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-
+    requires java.mail;
+    
     opens espol.proyectopoo_g8_p2 to javafx.fxml;
     exports espol.proyectopoo_g8_p2;
-    requires java.mail;
+    
 }
